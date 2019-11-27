88dc4eeb186683a43d67d950cb5e355e54934704
Johannes Schneider <jojo20872@gmail.com>
2019-11-27 18:56:21 -0500

    Check for edge cases in !stats

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -14,43 +14,112 @@ async def on_message(message):
 
 @client.event
 async def on_message(message):
-    if message.content.find("!hello") != -1:
+    
+    args = message.content.split()
+
+    if args[0] == '!hello':
         await message.channel.send("Hi") # If the user says !hello we will send back hi
 
-    if message.content.find("!stats") != -1:
+    # Prints the LoL stats of a specified player as an embed
+    if args[0] == '!stats':
+
+        # Ensure there is only 1 argument (name of player)
+        if len(args) != 2:
+            print("Invalid arguments!")
+            await message.channel.send("Invalid arguments!")
+            return
         
-        url = 'https://na.op.gg/summoner/userName=Dan12326'
+        # Find player op.gg page where stats are located
+        url = 'https://na.op.gg/summoner/userName=' + args[1]
 
         source = requests.get(url).text
-
         soup = BeautifulSoup(source, 'lxml')
 
-        name = soup.find('span', class_= "Name").text
+        # Ensure player exists
+        if soup.find('div', class_="SummonerNotFoundLayout") != None:
+            print("Invalid player!")
+            await message.channel.send("Player not found!")
+            return
 
+        # Get name
+        name = soup.find('span', class_= "Name")
+        if name != None:
+            name = name.text
+        else:
+            print("No name")
+            name = "N/A"
+            
+        # Get url to profile icon
         profile_icon = soup.find('img', class_= "ProfileImage")
+        if profile_icon != None:
+            profile_icon = profile_icon['src']
+        else: 
+            print("No profile icon")
+            profile_icon = "N/A"
 
-        level = soup.find('div', class_="ProfileIcon").span.text
+        # Get level
+        level = soup.find('div', class_= "ProfileIcon").span
+        if level != None:
+            level = level.text
+        else:
+            print("No level")
+            level = "N/A"
 
+        # Get url to tier icon
+        tier_icon = soup.find('div', class_= "SummonerRatingMedium").div.img
+        if tier_icon != None:
+            tier_icon = tier_icon['src']
+        else: 
+            print("No tier icon")
+            tier_icon = "N/A"        
+
+        # Player Tier Info
         tier = soup.find('div', class_= "TierRankInfo")
+        if tier != None:
 
-        tier_rank = tier.find('div', class_= "TierRank").text
+            # Get tier rank
+            tier_rank = tier.find('div', class_= "TierRank")
+            if tier_rank != None:
+                tier_rank = re.sub('\s+', '', tier_rank.text)    # remove whitespace
 
-        lp = re.sub('\s+', '', tier.find('span', class_= "LeaguePoints").text)
-        lp = re.sub('[^0-9]', '', lp)
+            # Get League Points
+            lp = tier.find('span', class_= "LeaguePoints")
+            if lp != None:
+                lp = re.sub('\s+', '', lp.text)
+                lp = re.sub('[^0-9]', '', lp)
+            else:
+                print("No lp")
+                lp = "N/A"
 
-        wins = tier.find('span', class_="wins").text
-        wins = re.sub('[^0-9]', '', wins)
+            # Get wins
+            wins = tier.find('span', class_="wins")
+            if wins != None:
+                wins = re.sub('[^0-9]', '', wins.text)          # remove non-numbers
+            else:
+                print("No wins")
+                wins = "N/A"
 
-        losses = tier.find('span', class_="losses").text
-        losses = re.sub('[^0-9]', '', losses)
+            # Get losses
+            losses = tier.find('span', class_="losses")
+            if losses != None:
+                losses = re.sub('[^0-9]', '', losses.text)
+            else:
+                print("No losses")
+                losses = "N/A"
 
-        winratio = tier.find('span', class_="winratio").text
-        winratio = re.sub('[^0-9]', '', winratio)
-        winratio = winratio + "%"
+            # Get win/loss ratio
+            winratio = tier.find('span', class_="winratio")
+            if winratio != None:
+                winratio = re.sub('[^0-9]', '', winratio.text)
+                winratio = winratio + "%"
+            else:
+                print("No winratio")
+                winratio = "N/A"
 
         await message.channel.send("Name: " + name)
-        await message.channel.send("Profile Image: " + profile_icon['src'])
+        await message.channel.send("Profile Image: " + profile_icon)
         await message.channel.send("Level: " + level)
+        await message.channel.send("Tier Image: " + tier_icon)
         await message.channel.send("Tier Rank: "  + tier_rank)
         await message.channel.send("LP: " + lp)
         await message.channel.send("Wins: " + wins)

885601e330c5ebf418c90895e757fcdc63fb150b
Johannes Schneider <jojo20872@gmail.com>
2019-11-28 10:06:55 -0500

    Use PlayerStats object for player info

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -3,6 +3,7 @@ import requests
 import re
 import discord
 from auth import token
+from Commands.PlayerStats import PlayerStats
 
 TOKEN = token
 
@@ -28,106 +29,26 @@ async def on_message(message):
             print("Invalid arguments!")
             await message.channel.send("Invalid arguments!")
             return
-        
-        # Find player op.gg page where stats are located
-        url = 'https://na.op.gg/summoner/userName=' + args[1]
 
-        source = requests.get(url).text
-        soup = BeautifulSoup(source, 'lxml')
+        # Create an object that contains player stats
+        player = PlayerStats(args[1])
 
-        # Ensure player exists
-        if soup.find('div', class_="SummonerNotFoundLayout") != None:
-            print("Invalid player!")
+        if (player.url == "Error"):
             await message.channel.send("Player not found!")
             return
-
-        # Get name
-        name = soup.find('span', class_= "Name")
-        if name != None:
-            name = name.text
-        else:
-            print("No name")
-            name = "N/A"
-            
-        # Get url to profile icon
-        profile_icon = soup.find('img', class_= "ProfileImage")
-        if profile_icon != None:
-            profile_icon = profile_icon['src']
-        else: 
-            print("No profile icon")
-            profile_icon = "N/A"
-
-        # Get level
-        level = soup.find('div', class_= "ProfileIcon").span
-        if level != None:
-            level = level.text
-        else:
-            print("No level")
-            level = "N/A"
-
-        # Get url to tier icon
-        tier_icon = soup.find('div', class_= "SummonerRatingMedium").div.img
-        if tier_icon != None:
-            tier_icon = tier_icon['src']
-        else: 
-            print("No tier icon")
-            tier_icon = "N/A"        
-
-        # Player Tier Info
-        tier = soup.find('div', class_= "TierRankInfo")
-        if tier != None:
-
-            # Get tier rank
-            tier_rank = tier.find('div', class_= "TierRank").text
-            # if tier_rank != None:
-                # tier_rank = re.sub('\s+', '', tier_rank.text)    # remove whitespace
-
-            # Get League Points
-            lp = tier.find('span', class_= "LeaguePoints")
-            if lp != None:
-                lp = re.sub('\s+', '', lp.text)
-                lp = re.sub('[^0-9]', '', lp)
-            else:
-                print("No lp")
-                lp = "N/A"
-
-            # Get wins
-            wins = tier.find('span', class_="wins")
-            if wins != None:
-                wins = re.sub('[^0-9]', '', wins.text)          # remove non-numbers
-            else:
-                print("No wins")
-                wins = "N/A"
-
-            # Get losses
-            losses = tier.find('span', class_="losses")
-            if losses != None:
-                losses = re.sub('[^0-9]', '', losses.text)
-            else:
-                print("No losses")
-                losses = "N/A"
-
-            # Get win/loss ratio
-            winratio = tier.find('span', class_="winratio")
-            if winratio != None:
-                winratio = re.sub('[^0-9]', '', winratio.text)
-                winratio = winratio + "%"
-            else:
-                print("No winratio")
-                winratio = "N/A"
 
         # Embed to display player stats
         embed = discord.Embed(
             color = discord.Color.teal()
         )
-        embed.set_author(name = name, url = url, icon_url = "https:" + profile_icon)
-        embed.set_thumbnail(url = "https:" + tier_icon)
-        embed.add_field(name = "Level", value = level, inline = True)
-        embed.add_field(name = "Tier Rank", value = tier_rank, inline = True)
-        embed.add_field(name = "League Points", value = lp, inline = True)
-        embed.add_field(name = "Wins", value = wins, inline = True)
-        embed.add_field(name = "Losses", value = losses, inline = True)
-        embed.add_field(name = "Win/Loss", value = winratio, inline = True)
+        embed.set_author(name = player.name, url = player.url, icon_url = "https:" + player.profile_icon)
+        embed.set_thumbnail(url = "https:" + player.tier_icon)
+        embed.add_field(name = "Level", value = player.level, inline = True)
+        embed.add_field(name = "Tier Rank", value = player.tier_rank, inline = True)
+        embed.add_field(name = "League Points", value = player.lp, inline = True)
+        embed.add_field(name = "Wins", value = player.wins, inline = True)
+        embed.add_field(name = "Losses", value = player.losses, inline = True)
+        embed.add_field(name = "Win/Loss", value = player.winratio, inline = True)
         embed.set_footer(text = "Stats found at op.gg")
 
         await message.channel.send(embed = embed)
